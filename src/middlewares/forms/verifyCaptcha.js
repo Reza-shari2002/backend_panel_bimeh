66c8980fe9b1f868a86799997eb8b3267bdc8ee7
Reza <shariatireza25@gmail.com>
2026-06-28 14:54:51 +0330

    update captcha verify

--- src/middlewares/forms/verifyCaptcha.js
+++ src/middlewares/forms/verifyCaptcha.js
@@ -4,12 +4,12 @@ require("dotenv").config();
 
 
 
+
+
 const verifyCaptcha = async (req, res, next) => {
   try {
-    
     const token = req.headers["x-captcha-token"];
 
-  
     if (!token) {
       return res.status(400).json({
         success: false,
@@ -17,32 +17,31 @@ const verifyCaptcha = async (req, res, next) => {
       });
     }
 
- 
+    const body = new URLSearchParams();
+    body.append("secret", process.env.TURNSTILE_SECRET_KEY);
+    body.append("response", token);
+
+
     const response = await axios.post(
       "https://challenges.cloudflare.com/turnstile/v0/siteverify",
+      body,
       {
-        secret: process.env.TURNSTILE_SECRET_KEY, 
-        response: token,
-        remoteip: req.ip, 
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
       }
     );
 
     const data = response.data;
 
-    
-    if (data.success) {
-      
-      return next();
-    } else {
-      
-      return res.status(403).json({
-        success: false,
-        message: "تاییدیه امنیتی (کپچا) نامعتبر است. لطفاً دوباره تلاش کنید.",
-        errors: data["error-codes"], 
-      });
-    }
+    if (data.success) return next();
+
+    return res.status(403).json({
+      success: false,
+      message: "تاییدیه امنیتی (کپچا) نامعتبر است. لطفاً دوباره تلاش کنید.",
+      errors: data["error-codes"],
+    });
   } catch (error) {
-  
     console.error("Turnstile Error:", error);
     return res.status(500).json({
       success: false,
